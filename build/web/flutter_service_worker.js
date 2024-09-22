12a7d21814b76e030d652a01af38b2d79146924b
Nandan Praveen <nandan.praveen007@gmail.com>
2024-09-22 12:21:37 -0400

    Update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ede955c94738a36872aaa89f63362310
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "86f8f9c5a23a11c19cd773df78bf52d2",
+"flutter_bootstrap.js": "70578cfda6a182a3fda25f2933c888da",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
